134e1c818aa096e095ecb04cb7a6009f37bd904a
SeiraLuna <SeiraLuna@outlook.com>
2018-08-06 17:25:48 -0700

    Cleaned-up w/ case-insensitive command support

--- Objects/Program.cs
+++ Objects/Program.cs
@@ -13,25 +13,6 @@ namespace Objects
     {
         static void Main(string[] args)
         {
-            /*
-            string filepath = @".\classList.txt";
-            Queue<string> loadList = new Queue<string>();
-
-            using (StreamReader sr = new StreamReader(filepath))
-            {
-                while (!sr.EndOfStream)
-                {
-                    loadList.Enqueue(sr.ReadLine());
-                }
-            }
-           */
-            /*
-             while(loadList.Count > 0)
-             {
-                 Profession profession = new Profession(loadList.Dequeue());
-             }
-             */
-
             Console.WriteLine("Please enter a commmand: ");
             string input = Console.ReadLine();
 
@@ -47,39 +28,27 @@ namespace Objects
         public static void Execute(string command)
         {
             UserCommand Commands = new UserCommand();
-            //Commands.CWL();
-
-            if (command.Contains(" "))
+            
+            try
             {
-                try
+                if (command.Contains(" "))
                 {
                     string filePath = command.Substring(command.IndexOf(" ") + 1);
                     command = command.Substring(0, command.IndexOf(" "));
-                    // below conditional works, but subsequent invoke returns error if case does not match.
-                    //if (UserCommand.CommandList.FindIndex(x => x.Equals(command, StringComparison.OrdinalIgnoreCase)) > -1)
-                    if (UserCommand.CommandList.Any(x => x.Key.Equals(command, StringComparison.OrdinalIgnoreCase)))
-                    //if (UserCommand.CommandList.Contains(command))
-                    {
-                        //UserCommand.CommandList[command].DynamicInvoke(filePath);                   
-                        Commands.GetType().GetMethod(command, new Type[] { typeof(string) }).Invoke(Commands, new object[] { filePath });
-                        //typeof(UserCommand).GetMethod(command).Invoke(null, new[] { filePath });
-                    }
+                    int commandIndex = UserCommand.CommandList.IndexOf((from x in UserCommand.CommandList where x.Equals(command, StringComparison.OrdinalIgnoreCase) select x).First());
+                    //if (UserCommand.CommandList.Any(x => x.Equals(command, StringComparison.OrdinalIgnoreCase)))          
+                    Commands.GetType().GetMethod(UserCommand.CommandList[commandIndex], new Type[] { typeof(string) }).Invoke(Commands, new object[] { filePath });
                 }
-
+                else
+                {
+                    int commandIndex = UserCommand.CommandList.IndexOf((from x in UserCommand.CommandList where x.Equals(command, StringComparison.OrdinalIgnoreCase) select x).First());
+                    Commands.GetType().GetMethod(UserCommand.CommandList[commandIndex], new Type[0]).Invoke(Commands, new object[0]);
+                }
             }
-            //else if (Command.CommandList.ContainsKey(command))
-            //Command.CommandList[command].DynamicInvoke();
-            //else if(UserCommand.CommandList.FindIndex(x => x.Equals(command, StringComparison.OrdinalIgnoreCase)) > -1)
-            else if (UserCommand.CommandList.Any(x => x.Key.Equals(command, StringComparison.OrdinalIgnoreCase)))
-            //else if (UserCommand.CommandList.Contains(command))
+            catch
             {
-                var cmd = from x in UserCommand.CommandList where x.Key.Equals(command, StringComparison.OrdinalIgnoreCase) select x.Key;
-                Commands.GetType().GetMethod(UserCommand.CommandList[command], new Type[0]).Invoke(Commands, new object[0]);
-                //typeof(UserCommand).GetMethod(command).Invoke(null, new[] { typeof(Nullable) });
-            }
-            else
                 Console.WriteLine("Invalid input");
+            } 
         }
-
     }
 }
--- Objects/UserCommands.cs
+++ Objects/UserCommands.cs
@@ -8,60 +8,37 @@ using System.IO;
 namespace Objects
 {
     public class UserCommand
-    {
-        /*
-        public UserCommand()
-        {
-        }
-        /*
+    {      
         public static List<string> CommandList = new List<string>()
         {
-            "Load",
-        };*/
-
-        public static Dictionary<string, string> CommandList = new Dictionary<string, string>()
-        {
-            { "Load", "Load" }
+            "Import",
         };
 
-       /*
-        public static Dictionary<string, Func<string, string>> CommandList = new Dictionary<string, Func<string, string>>()
-        {            
-            {"Load", (new Func<string,string>(Load))}
-        }; */
 
-
-
-        public static void Load(string directory)
+        public static void Import(string directory)
         {
             string filepath = directory;
-            List<string> loadList = new List<string>();
+            List<string> importList = new List<string>();
 
             using (StreamReader sr = new StreamReader(filepath))
             {
                 while (!sr.EndOfStream)
                 {
-                    loadList.Add(sr.ReadLine());
+                    importList.Add(sr.ReadLine());
                 }
             }
 
-            foreach(string s in loadList)
+            foreach(string s in importList)
             {
                 Console.WriteLine(s);
                 Profession profession = new Profession(s);
             }
         }
 
-        public static void Load()
+        public static void Import()
         {
-            string filepath = @".\classList.txt";
-            Load(filepath);
+            string filepath = @".\updateClassList.txt";
+            Import(filepath);
         }
-
-       // public void CWL()
-        //{
-         //   Console.WriteLine("looks like it instantiated...");
-        //}
     }
-
 }
